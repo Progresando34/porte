d7936dd31bfc63b3edb7b888c39eb4b8a6bed86e
Progresando24 <softwareprogresandoips@gmail.com>
2026-02-19 14:59:35 -0500

    Change path Controller api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\DocumentoEmpresaController;
 
+Route::get('/health', function (Request $request) {
+    return 'Health ... active';
+});
+
+
 Route::prefix('documentos')->group(function () {
 
     Route::get('/existe/{doc}', [DocumentoEmpresaController::class, 'existe']);
