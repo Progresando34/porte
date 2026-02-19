e004e8d696f243b8651f6dd447c19f026a7cfeff
Progresando24 <softwareprogresandoips@gmail.com>
2026-02-19 14:28:15 -0500

    Change path Controller api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DocumentoEmpresaController;
+use App\Http\Controllers\api\DocumentoEmpresaController;
 
 Route::prefix('documentos')->group(function () {
 
