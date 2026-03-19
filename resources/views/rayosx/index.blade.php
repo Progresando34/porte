a7742e8e9f937ca8074a1e79a8042b7adf86529f
roseroJuan299 <camiloroser20@gmail.com>
2026-03-19 17:54:00 -0500

    test indexx

---
+++ resources/views/rayosx/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+    <h2 class="mb-4">Listado de Rayos X</h2>
+
+    <a href="{{ route('rayosx.create') }}" class="btn btn-primary mb-3">
+        Nuevo Registro
+    </a>
+
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Cédula</th>
+                <th>Fecha</th>
+                <th>Archivo</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse($registros as $item)
+                <tr>
+                    <td>{{ $item->nombre }}</td>
+                    <td>{{ $item->cedula }}</td>
+                    <td>{{ $item->fecha_rx }}</td>
+                    <td>
+                        <a href="{{ asset('storage/' . $item->ruta) }}" target="_blank" class="btn btn-sm btn-success">
+                            Ver
+                        </a>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="4">No hay registros</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
+
+</div>
+@endsection
